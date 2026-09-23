39ba88580818dab4503adc2c99722c1d5243bde8
vaishu159878 <vaishnavinalawade2024@gmail.com>
2026-09-23 16:39:32 +0000

    Add GitHub Actions end-to-end CI/CD capstone

---
+++ test_app.py
@@ -0,0 +1,17 @@
+from app import app
+
+
+def test_home():
+    client = app.test_client()
+    response = client.get("/")
+    
+    assert response.status_code == 200
+    assert response.json["message"] == "GitHub Actions Capstone is running!"
+
+
+def test_health():
+    client = app.test_client()
+    response = client.get("/health")
+    
+    assert response.status_code == 200
+    assert response.json["status"] == "healthy"
